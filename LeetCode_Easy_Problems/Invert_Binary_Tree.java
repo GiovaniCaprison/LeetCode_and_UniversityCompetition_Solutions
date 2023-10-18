6f3911b8b11663d67cae76e81101e26830aad633
Louis Grennell <icantfindanadressname@gmail.com>
2023-10-18 14:46:29 +0100

    Create Invert_Binary_Tree.java

---
+++ LeetCode_Easy_Problems/Invert_Binary_Tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        reverse(root); // call the reverse function
+        return root; // return the root
+    }
+    static TreeNode reverse (TreeNode root) {
+
+        if(root == null) return root; // base case
+
+        reverse(root.left); // recursive call
+        reverse(root.right); // recursive call
+        TreeNode temp = root.left; // set dummy node to the left node
+        root.left = root.right; // swap the left node with the right
+        root.right = temp; // swap the right node with temp
+        return root; // return the root
+    }
+}
