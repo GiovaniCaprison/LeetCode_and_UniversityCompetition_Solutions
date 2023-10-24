2c79e48524ed42b3d67cac4e1f224fc712168481
Louis Grennell <icantfindanadressname@gmail.com>
2023-10-24 10:34:54 +0100

    DataStructures and Algorithms Lab

--- src/Competiton_Solutions/Monte_Carlo_e.java
+++ src/Competiton_Solutions/Monte_Carlo_e.java
@@ -1,26 +1,39 @@
 package Competiton_Solutions;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Monte_Carlo_e {
-    public static void main (String [] args) {
-
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int lim = scan.nextInt();
-        double e = Math.exp(1.0);
+        System.out.print("Enter the number of trials: ");
+        int N = scan.nextInt();
+        double estimatedE = estimateE(N);
+        double actualE = Math.E;
+        double absoluteError = Math.abs(estimatedE - actualE);
+        double relativeError = absoluteError / actualE;
+        double percentageError = relativeError * 100;
 
-        System.out.println("The value of e is: " + e);
-        System.out.println("The absolute error is: " + (e - absError(e,lim)));
+        System.out.println("Estimated value of e: " + estimatedE);
+        System.out.println("Actual value of e: " + actualE);
+        System.out.println("Percentage error: " + percentageError + "%");
     }
 
-    private static double absError(double e, int lim) {
 
-        double res = 0.0;
+    public static double estimateE(int N) {
+        Random rand = new Random();
+        int count = 0;
 
-        for(int i = 0; i < lim; i++) {
-            double rand = Math.random() * e;
-            res+= rand;
+        for (int i = 0; i < N; i++) {
+            double sum = 0;
+            int n = 0;
+            while (sum <= 1) {
+                sum += rand.nextDouble(); // Generates a random double between 0.0 and 1.0
+                n++;
+            }
+            count += n;
         }
-        return res/lim;
+
+        return (double) count / N;
     }
 }
--- src/Competiton_Solutions/Monte_Carlo_pi.java
+++ src/Competiton_Solutions/Monte_Carlo_pi.java
@@ -1,24 +1,31 @@
 package Competiton_Solutions;
 
+import java.util.Random;
 import java.util.Scanner;
+
 public class Monte_Carlo_pi {
-    public static void main (String [] args) {
-
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int lim = scan.nextInt();
-        double pi = Math.PI;
-        System.out.print(estimatePi(pi,lim));
-
+        int N = scan.nextInt(); // Number of random points
+        double estimatedPi = estimatePi(N);
+        System.out.println("Actual PI: " + Math.PI);
+        System.out.println("Estimated value of PI: " + estimatedPi);
+        System.out.println("Absolute error: " + Math.abs(1 - Math.PI/estimatedPi) + "%");
     }
 
-    private static double estimatePi(double pi, int lim) {
-        double res = 0.0;
-        for(int i = 0; i < lim; i++) {
-            double x = Math.random() * pi;
-            double y = Math.random() * pi;
-            res += (x*x + y*y <= pi*pi ? 1 : 0);
+    public static double estimatePi(int N) {
+        Random rand = new Random();
+        int M = 0; // Number of points inside the quarter circle
+
+        for (int i = 0; i < N; i++) {
+            double x = rand.nextDouble() * 2 - 1; // Random x coordinate between -1 and 1
+            double y = rand.nextDouble() * 2 - 1; // Random y coordinate between -1 and 1
+
+            if (x * x + y * y <= 1) {
+                M++;
+            }
         }
-        return res/lim;
-    }
 
+        return 4.0 * M / N;
+    }
 }
